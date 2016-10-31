ee0820368b338386e262df1e8382097c9792c158
Cristopher Rodrigues <cristopher.rodrigues993@gmail.com>
2016-10-31 14:46:26 -0200

    Oxxo

--- includes/class-wc-ebanx-gateway-utils.php
+++ includes/class-wc-ebanx-gateway-utils.php
@@ -10,4 +10,12 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 abstract class WC_Ebanx_Gateway_Utils {
     const COUNTRY_BRAZIL = 'br';
+
+    const CURRENCY_CODE_BRL = 'BRL';
+    const CURRENCY_CODE_USD = 'USD';
+    const CURRENCY_CODE_EUR = 'EUR';
+    const CURRENCY_CODE_PEN = 'PEN';
+    const CURRENCY_CODE_MXN = 'MXN';
+    const CURRENCY_CODE_COP = 'COP';
+    const CURRENCY_CODE_CLP = 'CLP';
 }
--- woocommerce-gateway-ebanx.php
+++ woocommerce-gateway-ebanx.php
@@ -1,94 +1,218 @@
 <?php
-/*
- * Plugin Name: WooCommerce EBANX Gateway
- * Plugin URI: https://wordpress.org/plugins/woocommerce-gateway-ebanx/
- * Description: Take credit card payments on your store using EBANX.
- * Author: Automattic
- * Author URI: http://woothemes.com/
- * Version: 3.0.2
- * Text Domain: woocommerce-gateway-ebanx
- * Domain Path: /languages
+/**
+ * Plugin Name: WooCommerce Ebanx.com
+ * Plugin URI: http://github.com/ebanx/woocommerce
+ * Description: Gateway de pagamento Ebanx.com para WooCommerce.
+ * Author: Woocommercer, Cristopher
+ * Author URI: https://ebanx.com/
+ * Version: 1.0.0
+ * License: GPLv2 or later
+ * Text Domain: woocommerce-ebanx
+ * Domain Path: /languages/
  *
- * Copyright (c) 2016 Automattic
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * @package WooCommerce_Ebanx
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-/**
- * Required minimums and constants
- */
-define( 'WC_EBANX_VERSION', '3.0.2' );
-define( 'WC_EBANX_MIN_PHP_VER', '5.3.0' );
-define( 'WC_EBANX_MIN_WC_VER', '2.5.0' );
-define( 'WC_EBANX_MAIN_FILE', __FILE__ );
-define( 'WC_EBANX_PLUGIN_URL', untrailingslashit( plugins_url( basename( plugin_dir_path( __FILE__ ) ), basename( __FILE__ ) ) ) );
+if ( ! class_exists( 'WC_Ebanx' ) ) :
 
-if ( ! class_exists( 'WC_EBANX' ) ) {
+	/**
+	 * WooCommerce WC_Ebanx main class.
+	 */
+	class WC_Ebanx {
 
-    class WC_EBANX {
+		/**
+		 * Plugin version.
+		 *
+		 * @var string
+		 */
+		const VERSION = '1.0.0';
 
-        /**
-         * @var Singleton The reference the *Singleton* instance of this class
-         */
-        private static $instance;
+        const DIR = __FILE__;
 
-        /**
-         * Returns the *Singleton* instance of this class.
-         *
-         * @return Singleton The *Singleton* instance.
-         */
-        public static function get_instance() {
-            if ( null === self::$instance ) {
-                self::$instance = new self();
+		/**
+		 * Instance of this class.
+		 *
+		 * @var object
+		 */
+		protected static $instance = null;
+
+        private static $log;
+
+		/**
+		 * Initialize the plugin public actions.
+		 */
+		private function __construct() {
+			// Load plugin text domain.
+			add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
+
+			// Checks with WooCommerce is installed.
+			if ( class_exists( 'WC_Payment_Gateway' ) ) {
+				$this->upgrade();
+				$this->includes();
+
+				add_filter( 'woocommerce_payment_gateways', array( $this, 'add_gateway' ) );
+				add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'plugin_action_links' ) );
+			} else {
+				add_action( 'admin_notices', array( $this, 'woocommerce_missing_notice' ) );
+			}
+		}
+
+		/**
+		 * Return an instance of this class.
+		 *
+		 * @return object A single instance of this class.
+		 */
+		public static function get_instance() {
+			// If the single instance hasn't been set, set it now.
+			if ( null === self::$instance ) {
+				self::$instance = new self;
+			}
+
+			return self::$instance;
+		}
+
+		/**
+		 * Includes.
+		 */
+		private function includes() {
+            include_once dirname( __FILE__ ) . '/includes/class-wc-ebanx-gateway-utils.php';
+			include_once dirname( __FILE__ ) . '/includes/class-wc-ebanx-gateway.php';
+			include_once dirname( __FILE__ ) . '/includes/class-wc-ebanx-my-account.php';
+			include_once dirname( __FILE__ ) . '/includes/class-wc-ebanx-banking-ticket-gateway.php';
+			include_once dirname( __FILE__ ) . '/includes/class-wc-ebanx-credit-card-gateway.php';
+            include_once dirname( __FILE__ ) . '/includes/class-wc-ebanx-oxxo-gateway.php';
+		}
+
+		/**
+		 * Load the plugin text domain for translation.
+		 */
+		public function load_plugin_textdomain() {
+			load_plugin_textdomain( 'woocommerce-ebanx', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+		}
+
+		/**
+		 * Get templates path.
+		 *
+		 * @return string
+		 */
+		public static function get_templates_path() {
+			return plugin_dir_path( __FILE__ ) . 'templates/';
+		}
+
+		/**
+		 * Add the gateway to WooCommerce.
+		 *
+		 * @param  array $methods WooCommerce payment methods.
+		 *
+		 * @return array
+		 */
+		public function add_gateway( $methods ) {
+			$methods[] = 'WC_Ebanx_Banking_Ticket_Gateway';
+			$methods[] = 'WC_Ebanx_Credit_Card_Gateway';
+            $methods[] = 'WC_Ebanx_Oxxo_Gateway';
+
+			return $methods;
+		}
+
+		/**
+		 * Action links.
+		 *
+		 * @param  array $links Plugin links.
+		 *
+		 * @return array
+		 */
+		public function plugin_action_links( $links ) {
+			$plugin_links = array();
+
+			$banking_ticket = 'wc_ebanx_banking_ticket_gateway';
+			$credit_card    = 'wc_ebanx_credit_card_gateway';
+            $oxxo           = 'wc_ebanx_oxxo_gateway';
+
+			$plugin_links[] = '<a href="' . esc_url( admin_url( 'admin.php?page=wc-settings&tab=checkout&section=' . $banking_ticket ) ) . '">' . __( 'Bank Slip Settings', 'woocommerce-ebanx' ) . '</a>';
+
+			$plugin_links[] = '<a href="' . esc_url( admin_url( 'admin.php?page=wc-settings&tab=checkout&section=' . $credit_card ) ) . '">' . __( 'Credit Card Settings', 'woocommerce-ebanx' ) . '</a>';
+
+            $plugin_links[] = '<a href="' . esc_url( admin_url( 'admin.php?page=wc-settings&tab=checkout&section=' . $oxxo ) ) . '">' . __( 'Oxxo Settings', 'woocommerce-ebanx' ) . '</a>';
+
+			return array_merge( $plugin_links, $links );
+		}
+
+		/**
+		 * WooCommerce fallback notice.
+		 */
+		public function woocommerce_missing_notice() {
+			include dirname( __FILE__ ) . '/includes/admin/views/html-notice-missing-woocommerce.php';
+		}
+
+		/**
+		 * Upgrade.
+		 *
+		 * @since 2.0.0
+		 */
+		private function upgrade() {
+			if ( is_admin() ) {
+				if ( $old_options = get_option( 'woocommerce_ebanx_settings' ) ) {
+					// Banking ticket options.
+                    $banking_ticket = array(
+                        'enabled'        => $old_options['enabled'],
+                        'title'          => 'Boleto bancário',
+                        'description'    => '',
+                        'api_key'        => $old_options['api_key'],
+                        'encryption_key' => $old_options['encryption_key'],
+                        'debug'          => $old_options['debug'],
+                    );
+
+                    // Oxxo options.
+                    $oxxo = array(
+                        'enabled'        => $old_options['enabled'],
+                        'title'          => 'Oxxo',
+                        'description'    => '',
+                        'api_key'        => $old_options['api_key'],
+                        'encryption_key' => $old_options['encryption_key'],
+                        'debug'          => $old_options['debug'],
+                    );
+
+					// Credit card options.
+					$credit_card = array(
+						'enabled'              => $old_options['enabled'],
+						'title'                => 'Cartão de crédito',
+						'description'          => '',
+						'api_key'              => $old_options['api_key'],
+						'encryption_key'       => $old_options['encryption_key'],
+						'checkout'             => 'no',
+						'max_installment'      => $old_options['max_installment'],
+						'smallest_installment' => $old_options['smallest_installment'],
+						'interest_rate'        => $old_options['interest_rate'],
+						'free_installments'    => $old_options['free_installments'],
+						'debug'                => $old_options['debug'],
+					);
+
+					update_option( 'woocommerce_ebanx-banking-ticket_settings', $banking_ticket );
+					update_option( 'woocommerce_ebanx-credit-card_settings', $credit_card );
+                    update_option( 'woocommerce_ebanx-oxxo_settings', $oxxo );
+
+					delete_option( 'woocommerce_ebanx_settings' );
+				}
+			}
+		}
+
+        public static function log( $message ) {
+            if ( empty( self::$log ) ) {
+                self::$log = new WC_Logger();
             }
-            return self::$instance;
+
+            self::$log->add( 'woocommerce-ebanx', $message );
+
+            if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+                error_log( $message );
+            }
         }
+	}
 
-        /**
-         * Private clone method to prevent cloning of the instance of the
-         * *Singleton* instance.
-         *
-         * @return void
-         */
-        private function __clone() {}
+	add_action( 'plugins_loaded', array( 'WC_Ebanx', 'get_instance' ) );
 
-        /**
-         * Private unserialize method to prevent unserializing of the *Singleton*
-         * instance.
-         *
-         * @return void
-         */
-        private function __wakeup() {}
-
-        /**
-         * Notices (array)
-         * @var array
-         */
-        public $notices = array();
-
-        /**
-         * Protected constructor to prevent creating a new instance of the
-         * *Singleton* via the `new` operator from outside of this class.
-         */
-        protected function __construct() {}
-    }
-
-    $GLOBALS['wc_ebanx'] = WC_EBANX::get_instance();
-
-}
-
+endif;
