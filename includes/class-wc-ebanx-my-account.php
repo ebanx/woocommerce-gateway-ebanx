c679ba6aebb584927ccfe7f63541562f55e6aa9a
Cezar Luiz <cezarluiz.c@gmail.com>
2016-11-14 15:48:15 -0200

    psr2

--- includes/class-wc-ebanx-my-account.php
+++ includes/class-wc-ebanx-my-account.php
@@ -5,49 +5,52 @@
  * @package WooCommerce_Ebanx/Frontend
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
 
 /**
  * WC_Ebanx_My_Account class.
  */
-class WC_Ebanx_My_Account {
+class WC_Ebanx_My_Account
+{
 
-	/**
-	 * Initialize my account actions.
-	 */
-	public function __construct() {
-		add_filter( 'woocommerce_my_account_my_orders_actions', array( $this, 'my_orders_banking_ticket_link' ), 10, 2 );
-	}
+    /**
+     * Initialize my account actions.
+     */
+    public function __construct()
+    {
+        add_filter('woocommerce_my_account_my_orders_actions', array($this, 'my_orders_banking_ticket_link'), 10, 2);
+    }
 
-	/**
-	 * Add banking ticket link/button in My Orders section on My Accout page.
-	 *
-	 * @param array    $actions Actions.
-	 * @param WC_Order $order   Order data.
-	 *
-	 * @return array
-	 */
-	public function my_orders_banking_ticket_link( $actions, $order ) {
-		if ( 'pagarme-banking-ticket' !== $order->payment_method ) {
-			return $actions;
-		}
+    /**
+     * Add banking ticket link/button in My Orders section on My Accout page.
+     *
+     * @param array    $actions Actions.
+     * @param WC_Order $order   Order data.
+     *
+     * @return array
+     */
+    public function my_orders_banking_ticket_link($actions, $order)
+    {
+        if ('pagarme-banking-ticket' !== $order->payment_method) {
+            return $actions;
+        }
 
-		if ( ! in_array( $order->get_status(), array( 'pending', 'on-hold' ), true ) ) {
-			return $actions;
-		}
+        if (!in_array($order->get_status(), array('pending', 'on-hold'), true)) {
+            return $actions;
+        }
 
-		$data = get_post_meta( $order->id, '_wc_ebanx_transaction_data', true );
-		if ( ! empty( $data['boleto_url'] ) ) {
-			$actions[] = array(
-				'url'  => $data['boleto_url'],
-				'name' => __( 'Print Banking Ticket', 'woocommerce-ebanx' ),
-			);
-		}
+        $data = get_post_meta($order->id, '_wc_ebanx_transaction_data', true);
+        if (!empty($data['boleto_url'])) {
+            $actions[] = array(
+                'url'  => $data['boleto_url'],
+                'name' => __('Print Banking Ticket', 'woocommerce-ebanx'),
+            );
+        }
 
-		return $actions;
-	}
+        return $actions;
+    }
 }
 
 new WC_Ebanx_My_Account();
--- includes/class-wc-ebanx-safetypay-gateway.php
+++ includes/class-wc-ebanx-safetypay-gateway.php
@@ -1,37 +1,40 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
 
-class WC_Ebanx_Safetypay_Gateway extends WC_Ebanx_Redirect_Gateway  {
+class WC_Ebanx_Safetypay_Gateway extends WC_Ebanx_Redirect_Gateway
+{
 
-	public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
 
         $this->id                   = 'ebanx-safetypay';
-		$this->icon                 = apply_filters( 'wc_ebanx_safetypay_icon', false );
-		$this->has_fields           = true;
-		$this->method_title         = __( 'EBANX - Safetypay', 'woocommerce-ebanx' );
-		$this->method_description   = __( 'Accept Safetypay payments using EBANX.', 'woocommerce-ebanx' );
-		$this->view_transaction_url = 'https://dashboard.ebanx.com/#/transactions/%s';
+        $this->icon                 = apply_filters('wc_ebanx_safetypay_icon', false);
+        $this->has_fields           = true;
+        $this->method_title         = __('EBANX - Safetypay', 'woocommerce-ebanx');
+        $this->method_description   = __('Accept Safetypay payments using EBANX.', 'woocommerce-ebanx');
+        $this->view_transaction_url = 'https://dashboard.ebanx.com/#/transactions/%s';
 
-		$this->init_form_fields();
+        $this->init_form_fields();
 
-		$this->title          = 'Safetypay';
+        $this->title          = 'Safetypay';
         $this->description    = 'Safetypaydescription';
-		$this->api_key        = $this->get_option( 'api_key' );
-		$this->encryption_key = $this->get_option( 'encryption_key' );
-        $this->debug          = $this->get_option( 'debug' );
+        $this->api_key        = $this->get_option('api_key');
+        $this->encryption_key = $this->get_option('encryption_key');
+        $this->debug          = $this->get_option('debug');
 
-        if ( 'yes' === $this->debug ) {
+        if ('yes' === $this->debug) {
             $this->log = new WC_Logger();
         }
 
-		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
-	}
+        add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
+    }
 
-    public function is_available() {
+    public function is_available()
+    {
         return parent::is_available() && ($this->getTransactionAddress('country') == WC_Ebanx_Gateway_Utils::COUNTRY_PERU);
     }
 
@@ -40,84 +43,87 @@ class WC_Ebanx_Safetypay_Gateway extends WC_Ebanx_Redirect_Gateway  {
      * Admin page.
      */
     /*public function admin_options() {
-        include dirname( __FILE__ ) . '/admin/views/notices/html-notice-country-not-supported.php';
+    include dirname( __FILE__ ) . '/admin/views/notices/html-notice-country-not-supported.php';
     }*/
 
-	public function init_form_fields() {
-		$this->form_fields = array(
-			'enabled' => array(
-				'title'   => __( 'Enable/Disable', 'woocommerce-ebanx' ),
-				'type'    => 'checkbox',
-				'label'   => __( 'Enable EBANX Safetypay', 'woocommerce-ebanx' ),
-				'default' => 'no',
-			),
-			'integration' => array(
-				'title'       => __( 'Integration Settings', 'woocommerce-ebanx' ),
-				'type'        => 'title',
-				'description' => '',
-			),
-			'api_key' => array(
-				'title'             => __( 'EBANX API Key', 'woocommerce-ebanx' ),
-				'type'              => 'text',
-				'description'       => sprintf( __( 'Please enter your EBANX API Key. This is needed to process the payment and notifications. Is possible get your API Key in %s.', 'woocommerce-ebanx' ), '<a href="https://dashboard.ebanx.com/">' . __( 'EBANX Dashboard > My Account page', 'woocommerce-ebanx' ) . '</a>' ),
-				'default'           => '',
-				'custom_attributes' => array(
-					'required' => 'required',
-				),
-			),
-			'encryption_key' => array(
-				'title'             => __( 'EBANX Encryption Key', 'woocommerce-ebanx' ),
-				'type'              => 'text',
-				'description'       => sprintf( __( 'Please enter your EBANX Encryption key. This is needed to process the payment. Is possible get your Encryption Key in %s.', 'woocommerce-ebanx' ), '<a href="https://dashboard.ebanx.com/">' . __( 'EBANX Dashboard > My Account page', 'woocommerce-ebanx' ) . '</a>' ),
-				'default'           => '',
-				'custom_attributes' => array(
-					'required' => 'required',
-				),
-			),
-            'testing' => array(
-                'title'       => __( 'Gateway Testing', 'woocommerce-ebanx' ),
+    public function init_form_fields()
+    {
+        $this->form_fields = array(
+            'enabled'        => array(
+                'title'   => __('Enable/Disable', 'woocommerce-ebanx'),
+                'type'    => 'checkbox',
+                'label'   => __('Enable EBANX Safetypay', 'woocommerce-ebanx'),
+                'default' => 'no',
+            ),
+            'integration'    => array(
+                'title'       => __('Integration Settings', 'woocommerce-ebanx'),
                 'type'        => 'title',
                 'description' => '',
             ),
-            'testmode' => array(
-                'type'          => 'checkbox',
-                'title'         => __( 'Test Mode', 'woocommerce-ebanx' ),
-                'description'   => __( 'Use the test mode on EBANX dashboard to verify everything works before going live.', 'woocommerce-ebanx' ),
-                'label'         => __( 'Turn on testing', 'woocommerce-ebanx' ),
-                'default'       => 'no'
+            'api_key'        => array(
+                'title'             => __('EBANX API Key', 'woocommerce-ebanx'),
+                'type'              => 'text',
+                'description'       => sprintf(__('Please enter your EBANX API Key. This is needed to process the payment and notifications. Is possible get your API Key in %s.', 'woocommerce-ebanx'), '<a href="https://dashboard.ebanx.com/">' . __('EBANX Dashboard > My Account page', 'woocommerce-ebanx') . '</a>'),
+                'default'           => '',
+                'custom_attributes' => array(
+                    'required' => 'required',
+                ),
             ),
-            'debug' => array(
-                'title'       => __( 'Debug Log', 'woocommerce-ebanx' ),
+            'encryption_key' => array(
+                'title'             => __('EBANX Encryption Key', 'woocommerce-ebanx'),
+                'type'              => 'text',
+                'description'       => sprintf(__('Please enter your EBANX Encryption key. This is needed to process the payment. Is possible get your Encryption Key in %s.', 'woocommerce-ebanx'), '<a href="https://dashboard.ebanx.com/">' . __('EBANX Dashboard > My Account page', 'woocommerce-ebanx') . '</a>'),
+                'default'           => '',
+                'custom_attributes' => array(
+                    'required' => 'required',
+                ),
+            ),
+            'testing'        => array(
+                'title'       => __('Gateway Testing', 'woocommerce-ebanx'),
+                'type'        => 'title',
+                'description' => '',
+            ),
+            'testmode'       => array(
                 'type'        => 'checkbox',
-                'label'       => __( 'Enable logging', 'woocommerce-ebanx' ),
+                'title'       => __('Test Mode', 'woocommerce-ebanx'),
+                'description' => __('Use the test mode on EBANX dashboard to verify everything works before going live.', 'woocommerce-ebanx'),
+                'label'       => __('Turn on testing', 'woocommerce-ebanx'),
                 'default'     => 'no',
-                'description' => sprintf( __( 'Log EBANX events, such as API requests. You can check the log in %s', 'woocommerce-ebanx' ), '<a href="' . esc_url( admin_url( 'admin.php?page=wc-status&tab=logs&log_file=' . esc_attr( $this->id ) . '-' . sanitize_file_name( wp_hash( $this->id ) ) . '.log' ) ) . '">' . __( 'System Status &gt; Logs', 'woocommerce-ebanx' ) . '</a>' ),
             ),
-		);
-	}
+            'debug'          => array(
+                'title'       => __('Debug Log', 'woocommerce-ebanx'),
+                'type'        => 'checkbox',
+                'label'       => __('Enable logging', 'woocommerce-ebanx'),
+                'default'     => 'no',
+                'description' => sprintf(__('Log EBANX events, such as API requests. You can check the log in %s', 'woocommerce-ebanx'), '<a href="' . esc_url(admin_url('admin.php?page=wc-status&tab=logs&log_file=' . esc_attr($this->id) . '-' . sanitize_file_name(wp_hash($this->id)) . '.log')) . '">' . __('System Status &gt; Logs', 'woocommerce-ebanx') . '</a>'),
+            ),
+        );
+    }
 
-	public function thankyou_page( $order_id ) {
-		$order = wc_get_order( $order_id );
-		$data  = get_post_meta( $order_id, '_wc_ebanx_transaction_data', true );
+    public function thankyou_page($order_id)
+    {
+        $order = wc_get_order($order_id);
+        $data  = get_post_meta($order_id, '_wc_ebanx_transaction_data', true);
 
         // TODO: how do this?
 
-		if ( isset( $data['installments'] ) && in_array( $order->get_status(), array( 'processing', 'on-hold' ), true ) ) {
-			wc_get_template(
-				'safetypay/payment-instructions.php',
-				array(
-          'title' => $this->title,
-          'description' => $this->description
-        ),
-				'woocommerce/ebanx/',
-				WC_Ebanx::get_templates_path()
-			);
-		}
-	}
+        if (isset($data['installments']) && in_array($order->get_status(), array('processing', 'on-hold'), true)) {
+            wc_get_template(
+                'safetypay/payment-instructions.php',
+                array(
+                    'title'       => $this->title,
+                    'description' => $this->description,
+                ),
+                'woocommerce/ebanx/',
+                WC_Ebanx::get_templates_path()
+            );
+        }
+    }
 
-    public function payment_fields() {
-        if ( $description = $this->get_description() ) {
-            echo wp_kses_post( wpautop( wptexturize( $description ) ) );
+    public function payment_fields()
+    {
+        if ($description = $this->get_description()) {
+            echo wp_kses_post(wpautop(wptexturize($description)));
         }
 
         $cart_total = $this->get_order_total();
@@ -125,29 +131,31 @@ class WC_Ebanx_Safetypay_Gateway extends WC_Ebanx_Redirect_Gateway  {
         wc_get_template(
             'safetypay/payment-form.php',
             array(
-                'title' => $this->title,
-                'description' => $this->description
+                'title'       => $this->title,
+                'description' => $this->description,
             ),
             'woocommerce/ebanx/',
             WC_Ebanx::get_templates_path()
         );
     }
 
-	protected function request_data($order) {
+    protected function request_data($order)
+    {
         if (!isset($_POST['safetypay']) || !in_array($_POST['safetypay'], WC_Ebanx_Gateway_Utils::$TYPES_SAFETYPAY_ALLOWED)) {
             throw new Exception("Invalid safetypay type.");
         }
 
         $data = parent::request_data($order);
 
-        $data['payment']['payment_type_code'] = 'safetypay-'.$_POST['safetypay'];
+        $data['payment']['payment_type_code'] = 'safetypay-' . $_POST['safetypay'];
 
         return $data;
     }
-		
-		protected function save_order_meta_fields( $order, $request ) {
-      parent::save_order_meta_fields($order, $request);
-      
-      // TODO: What are the fields necessaries by this payment method?
+
+    protected function save_order_meta_fields($order, $request)
+    {
+        parent::save_order_meta_fields($order, $request);
+
+        // TODO: What are the fields necessaries by this payment method?
     }
 }
