d5d6b3c96dcb80950be4610bafb43388fc4983ad
Cristopher Rodrigues <cristopher.rodrigues993@gmail.com>
2016-11-18 16:26:19 -0200

    #57

--- assets/js/checkout-fields.js
+++ assets/js/checkout-fields.js
@@ -1,4 +1,7 @@
 jQuery( function($) {
+	$(document).find("#ebanx_billing_brazil_birth_date").mask('00/00/0000');
+	$(document).find("#ebanx_billing_brazil_document").mask('000.000.000-00');
+
 	var ebanxBillingContainers = function() {
 		return jQuery('.woocommerce-checkout').find('p').each(function() {
 			if(this.id.match(/^ebanx_billing_.*$/)) {
