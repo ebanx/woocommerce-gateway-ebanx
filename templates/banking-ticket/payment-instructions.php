f2b7c62dcc0ec41bd68ef55dba3969c2674a4e87
Cristopher Rodrigues <cristopher.rodrigues993@gmail.com>
2016-11-24 12:50:27 -0200

    fix general

--- templates/banking-ticket/payment-instructions.php
+++ templates/banking-ticket/payment-instructions.php
@@ -12,15 +12,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-<div class="woocommerce-message">
-	<span><a class="button" href="<?php echo esc_url( $url ); ?>" target="_blank"><?php esc_html_e( 'Pay the banking ticket', 'woocommerce-pagarme' ); ?></a><?php esc_html_e( 'Please click in the following button to view your banking ticket.', 'woocommerce-pagarme' ); ?><br /><?php esc_html_e( 'You can print and pay in your internet banking or in a lottery retailer.', 'woocommerce-pagarme' ); ?><br /><?php esc_html_e( 'After we receive the banking ticket payment confirmation, your order will be processed.', 'woocommerce-pagarme' ); ?></span>
-</div>
-
-<div class="woocommerce-message">
-	<span>Pay this banking ticket until: <?php echo $due_date ?></span>
-</div>
-
-<div class="woocommerce-message">
-	<p>You can pay this banking ticket simply copying the barcode below.</p>
-  <span><strong>Barcode: </strong> <?php echo $barcode ?></span>
+<div>
+	<iframe src="<?php echo $url; ?>" style="width: 100%; height: 1000px; border: 0px;"></iframe>
 </div>
