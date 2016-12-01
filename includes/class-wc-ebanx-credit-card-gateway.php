3db9aeb1c59824f795d69ec1c4914dc6fd5984cb
Cristopher Rodrigues <cristopher.rodrigues993@gmail.com>
2016-12-01 17:37:48 -0200

    fix

--- includes/class-wc-ebanx-credit-card-gateway.php
+++ includes/class-wc-ebanx-credit-card-gateway.php
@@ -156,7 +156,6 @@ class WC_Ebanx_Credit_Card_Gateway extends WC_Ebanx_Gateway
         update_post_meta($order->id, 'Number of Instalments', $request->payment->instalments);
     }
 
-<<<<<<< f3738405362a9aa6ee5c800dc7e26ee5fb9972d7
     protected function save_user_meta_fields($order)
     {
         parent::save_user_meta_fields($order);
@@ -183,13 +182,6 @@ class WC_Ebanx_Credit_Card_Gateway extends WC_Ebanx_Gateway
             $cards[] = $card;
 
             update_user_meta($this->userId, '__ebanx_credit_card_token', $cards);
-=======
-    protected function save_user_meta_fields($order) {
-        parent::save_user_meta_fields($order);
-
-        if($this->userId) {
-            update_user_meta($this->userId, '__ebanx_credit_card_token', $_POST['ebanx_token']);
->>>>>>> continue
         }
     }
 }
