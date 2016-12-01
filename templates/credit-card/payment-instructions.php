e9cd5d0489dd4070c9e84a41a5bdd2609bc2bdf3
Cezar Luiz <cezarluiz.c@gmail.com>
2016-12-01 11:52:39 -0200

    Passed PSR2 filter on files

--- templates/credit-card/payment-instructions.php
+++ templates/credit-card/payment-instructions.php
@@ -7,13 +7,13 @@
  * @version 2.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+    exit;
 }
 
 ?>
 <!-- TODO:Create a better design to do this -->
 
 <div class="woocommerce-message">
-	<span><?php printf( wp_kses( __( 'Payment successfully made using %1$s credit card in %2$s.', 'woocommerce-pagarme' ), array( 'strong' => array() ) ), '<strong>' . esc_html( ucwords($card_brand) ) . '</strong>', '<strong>' . intval( $instalments ) . 'x</strong>' ); ?></span>
+	<span><?php printf(wp_kses(__('Payment successfully made using %1$s credit card in %2$s.', 'woocommerce-pagarme'), array('strong' => array())), '<strong>' . esc_html(ucwords($card_brand)) . '</strong>', '<strong>' . intval($instalments) . 'x</strong>');?></span>
 </div>
