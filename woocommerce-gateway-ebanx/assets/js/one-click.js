e10b363292aa2f11d0e42b136f1306a460fe8060
Cezar Luiz <cezarluiz.c@gmail.com>
2016-12-28 19:42:28 -0200

    NEw style of one click payment

--- woocommerce-gateway-ebanx/assets/js/one-click.js
+++ woocommerce-gateway-ebanx/assets/js/one-click.js
@@ -1,24 +1,30 @@
-jQuery( document ).ready( function($){
-    var button = $('.ebanx-one-click-button'),
-        hidden = $('[name="ebanx_one_click"]');
+jQuery(document).ready(function ($) {
+    var button = $('#ebanx-one-click-button');
+    var hidden = $('#ebanx-one-click');
+    var close = $('.ebanx-one-click-close-button');
+    var tooltip = $('.ebanx-one-click-tooltip');
 
-    button.click(function() {
-        event.preventDefault();
-        $('#ebanx-one-click-modal').show();
+    button.on('click', function (e) {
+      e.preventDefault();
+      tooltip.toggleClass('is-active');
     });
 
-    $("#ebanx-one-click-pay").click(function() {
-        hidden.val('is_one_click');
-        var cardUse = $('.ebanx-card-use:checked');
-        var cardUseCvv = $('input[name="ebanx_one_click_cvv"]').val();
-
-        if (!cardUse || !cardUse.val() || !cardUseCvv|| cardUseCvv.trim().length !== 3) {
-            return false;
-        }
-
-        $('input[name="ebanx_one_click_token"]').val(cardUse.val());
-
-        $("form.cart").submit();
+    close.on('click', function (e) {
+      e.preventDefault();
+      tooltip.removeClass('is-active');
     });
 
+    $('.ebanx-one-click-pay').on('click', function (e) {
+      e.preventDefault();
+
+      hidden.val('is_one_click');
+      var cardSelected = $('.ebanx-card-use:checked');
+      var cvv = $('#ebanx-one-click-cvv-input').val();
+
+      if (!!cvv.length === false) {
+        return false;
+      }
+
+      $('form.cart').submit();
+    });
 });
--- woocommerce-gateway-ebanx/templates/credit-card/one-click.php
+++ woocommerce-gateway-ebanx/templates/credit-card/one-click.php
@@ -5,42 +5,43 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 global $product;
+$i = 0;
 
 ?>
 
-<div class="clear"></div>
-<div class="ebanx-one-click-wrapper">
+<?php if ($cards): ?>
+    <div class="clear"></div>
+    <div class="ebanx-one-click-container">
+        <input type="hidden" name="ebanx_one_click" id="ebanx-one-click" value />
 
-<!--    --><?php //do_action( 'ebanx_before_one_click_button' ); ?>
+        <div class="ebanx-one-click-button-container">
+            <button id="ebanx-one-click-button" class="ebanx-one-click-button button" type="button"><?php _e('One-Click Purchase', 'woocommerce-gateway-ebanx') ?></button>
 
-    <input type="hidden" name="ebanx_one_click" value />
-    <input type="hidden" name="ebanx_one_click_token" value />
-    <button type="submit" class="ebanx-one-click-button button"><span class="button-label"><?php echo $label ?></span></button>
+            <div class="ebanx-one-click-tooltip">
+                <button class="ebanx-one-click-close-button"></button>
 
-<!--    --><?php //do_action( 'ebanx_after_one_click_button' ); ?>
+                <h3><?php echo $t['title'] ?></h3>
+                <div class="ebanx-one-click-cards">
+                    <?php foreach ($cards as $key => $card): ?>
+                        <label class="ebanx-one-click-card">
+                            <input type="radio" class="ebanx-one-click-card-radio" name="ebanx_one_click_token" value="<?php echo $card->token ?>" <?php echo $i === 0 ? 'checked="checked"' : '' ?> />
+                            <img src="<?php echo PLUGIN_DIR_URL . "assets/images/icons/$card->brand.png" ?>" height="20" />
+                            <span>&bull;&bull;&bull;&bull; <?php echo substr($card->masked_number, -4) ?></span>
+                        </label>
+                    <?php $i++; endforeach; ?>
+                </div>
 
-</div>
+                <div class="ebanx-one-click-cvv">
+                    <label><?php echo $t['cvv'] ?> *</label>
+                    <input type="text" maxlength="4" minlength="3" class="ebanx-one-click-cvv-input" id="ebanx-one-click-cvv-input" name="ebanx_one_click_cvv" placeholder="CVV">
+                </div>
 
-<div class="modal" id="ebanx-one-click-modal" style="display: none;">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close">&times;</button>
-                <h4 class="modal-title" id="myModalLabel">Choose card</h4>
-            </div>
-            <div class="modal-body">
-                <?php if ($cards): ?>
-                    <?php foreach ($cards as $card) : ?>
-                        <input type="radio" class="input-radio ebanx-card-use" value="<?php echo $card->token; ?>" /> <?php echo $card->brand . " " . $card->masked_number; ?>
-                    <?php endforeach; ?>
-                <?php endif ?>
-                <input class="input-text" maxlength="3" name="ebanx_one_click_cvv" type="text" autocomplete="off" placeholder="CVV"/>
-                <?php include 'installments.php';?>
-            </div>
-            <div class="modal-footer">
-<!--                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>-->
-                <button type="button" class="btn btn-primary" id="ebanx-one-click-pay">Pay</button>
+                <div class="ebanx-one-click-installments">
+                    <?php include 'installments.php';?>
+                </div>
+
+                <button class="ebanx-one-click-pay button" type="button"><?php echo $t['pay'] ?></button>
             </div>
         </div>
     </div>
-</div>
+<?php endif ?>
