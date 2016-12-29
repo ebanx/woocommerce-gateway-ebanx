6c676e036386835f64022367105fd75b5148fdbb
danielnass <daniel@ebanx.com>
2016-12-29 21:38:35 -0200

    Change javascript to support old browsers

--- woocommerce-gateway-ebanx/assets/js/order-received.js
+++ woocommerce-gateway-ebanx/assets/js/order-received.js
@@ -1,19 +1,22 @@
-window.addEventListener('DOMContentLoaded', function(){
+jQuery(document).ready(function ($) {
     var clipboard = new Clipboard('.ebanx-button--copy');
 
     clipboard.on('success', function(e) {
-        e.trigger.classList.add('ebanx-button--copy-success');
+        var $target = $(e.trigger);
+        $target.addClass('ebanx-button--copy-success');
 
         setTimeout(function() {
-            e.trigger.classList.remove('ebanx-button--copy-success');
+            $target.removeClass('ebanx-button--copy-success');
         }, 2000);
     });
 
     clipboard.on('error', function(e) {
-        e.trigger.classList.add('ebanx-button--copy-error');
+        var $target = $(e.trigger);
+
+        $target.addClass('ebanx-button--copy-error');
 
         setTimeout(function() {
-            e.trigger.classList.remove('ebanx-button--copy-error');
+            $target.addClass('ebanx-button--copy-error');
         }, 2000);
     });
-}, true);
+});
