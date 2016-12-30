bc0a02b20bc4b2748d200a5631fcb9592c51b96d
danielnass <daniel@ebanx.com>
2016-12-30 15:56:50 -0200

    Thank you page for pago efectivo

--- woocommerce-gateway-ebanx/templates/pagoefectivo/payment-instructions.php
+++ woocommerce-gateway-ebanx/templates/pagoefectivo/payment-instructions.php
@@ -12,7 +12,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 ?>
-
-<div class="woocommerce-message">
-	<span><?php printf( wp_kses( __( 'Payment successfully made using %1$s pagoefectivo in %2$s.', 'woocommerce-gateway-ebanx' ), array( 'strong' => array() ) ), '' ); ?></span>
+<?php var_dump( get_defined_vars() ); ?>
+<div class="ebanx-order__desc">
+    <p>Acabamos de confirmar la operación y procesaremos tu orden. Imprime tu cupón y acércate a cualquier centro autorizado para realizar tu pago.</p>
+    <p>Una copia del cupón fue enviada al correo electrónico:</p>
+    <p><strong><?=$customer_email ?></strong></p>
+    <p>Si tienes dudas, por favor escribe a <a href="mailto:soporte@ebanx.com">soporte@ebanx.com</a>.</p>
 </div>
+
