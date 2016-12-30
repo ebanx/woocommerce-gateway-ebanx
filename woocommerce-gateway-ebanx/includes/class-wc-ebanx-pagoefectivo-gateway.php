fd5eba656d70becb2f9629a4dd472aae1cc271bc
danielnass <daniel@ebanx.com>
2016-12-30 19:20:50 -0200

    Change parent class to redirect to thank you page in pago efectivo

--- woocommerce-gateway-ebanx/includes/class-wc-ebanx-pagoefectivo-gateway.php
+++ woocommerce-gateway-ebanx/includes/class-wc-ebanx-pagoefectivo-gateway.php
@@ -4,7 +4,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-class WC_EBANX_Pagoefectivo_Gateway extends WC_EBANX_Redirect_Gateway
+class WC_EBANX_Pagoefectivo_Gateway extends WC_EBANX_Gateway
 {
 
     public function __construct()
