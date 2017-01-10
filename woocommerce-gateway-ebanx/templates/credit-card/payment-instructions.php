3223c6f419fd5769a3a7235910c035e06f1b98f2
Roger Cruz <spark.crz@gmail.com>
2017-01-10 19:21:12 -0200

    Added key string to template

--- woocommerce-gateway-ebanx/templates/credit-card/payment-instructions.php
+++ woocommerce-gateway-ebanx/templates/credit-card/payment-instructions.php
@@ -17,5 +17,5 @@ if (!defined('ABSPATH')) {
 <?php if($auto_capture){ ?>
 <p><?php printf(wp_kses(__('Payment successfully made using %1$s credit card in %2$s.', 'woocommerce-gateway-ebanx'), array('strong' => array())), '<strong>' . esc_html(ucwords($card_brand)) . '</strong>', '<strong>' . intval($instalments) . 'x</strong>');?></p>
 <?php } else { ?>
-<p><?php printf(wp_kses(__('%1$s, your payment is on the way. Wait for a mail confirmation at %2$s.', 'woocommerce-gateway-ebanx'), array('strong' => array())), '<strong>'.esc_html(ucwords($username)).'</strong>', '<strong>'.esc_html($useremail).'</strong>');?></p>
+<p><?php printf(wp_kses(__('%1$s, your payment is on the way. Wait for mail confirmation on %2$s.', 'woocommerce-gateway-ebanx'), array('strong' => array())), '<strong>'.esc_html(ucwords($username)).'</strong>', '<strong>'.esc_html($useremail).'</strong>');?></p>
 <?php } ?>
