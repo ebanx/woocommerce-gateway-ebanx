bcdea2ba3bba158037ec994866e36c7bf2407a2b
Guilherme Pressutto <gpressutto5@gmail.com>
2017-03-29 19:43:31 -0300

    Changed method name

--- woocommerce-gateway-ebanx/services/class-wc-ebanx-flash.php
+++ woocommerce-gateway-ebanx/services/class-wc-ebanx-flash.php
@@ -15,7 +15,7 @@ class WC_EBANX_Flash {
 	 *
 	 * @return void
 	 */
-	public static function display_messages() {
+	public static function enqueue_admin_messages() {
 		$flash_messages = self::get_messages();
 		$notices = new WC_EBANX_Notice();
 		foreach ($flash_messages as $flash_message) {
@@ -27,34 +27,34 @@ class WC_EBANX_Flash {
 			}
 			$notices->enqueue();
 		}
-    }
+	}
 
-    /**
-     * Enqueue a message to the next admin_init
-     *
-     * @param  string  $message     The message to enqueue
-     * @param  string  $type        The notice type
-     * @param  boolean $dismissible If the notice will be dismissible
-     * @return void
-     */
-    public static function enqueue_message($message, $type = 'error', $dismissible = false) {
-    	$flash_messages = maybe_unserialize(get_option(self::KEY, array()));
-    	$flash_messages[] = [
-    		'message' => $message,
-    		'type' => $type,
-    		'dismissible' => $dismissible
-    	];
-    	update_option(self::KEY, $flash_messages);
-    }
+	/**
+	 * Enqueue a message to the next admin_init
+	 *
+	 * @param  string  $message     The message to enqueue
+	 * @param  string  $type        The notice type
+	 * @param  boolean $dismissible If the notice will be dismissible
+	 * @return void
+	 */
+	public static function add_message($message, $type = 'error', $dismissible = false) {
+		$flash_messages = maybe_unserialize(get_option(self::KEY, array()));
+		$flash_messages[] = array(
+			'message' => $message,
+			'type' => $type,
+			'dismissible' => $dismissible
+		);
+		update_option(self::KEY, $flash_messages);
+	}
 
-    /**
-     * Returns all the unqueued flash messages in an array
-     *
-     * @return array All the enqueued flash messages
-     */
-    public static function get_messages(){
-    	$flash_messages = maybe_unserialize(get_option(self::KEY, array()));
-    	delete_option(self::KEY);
-    	return $flash_messages;
-    }
+	/**
+	 * Returns all the unqueued flash messages in an array
+	 *
+	 * @return array All the enqueued flash messages
+	 */
+	public static function get_messages(){
+		$flash_messages = maybe_unserialize(get_option(self::KEY, array()));
+		delete_option(self::KEY);
+		return $flash_messages;
+	}
 }
--- woocommerce-gateway-ebanx/woocommerce-gateway-ebanx.php
+++ woocommerce-gateway-ebanx/woocommerce-gateway-ebanx.php
@@ -95,7 +95,7 @@ if (!class_exists('WC_EBANX')) {
 			add_action('init', array($this, 'ebanx_router'));
 			add_action('admin_footer', array($this, 'render_static_assets'), 0);
 			add_action('admin_init', array($this, 'ebanx_sidebar_shortcut'));
-			add_action('admin_init', array('WC_EBANX_Flash', 'display_messages'));
+			add_action('admin_init', array('WC_EBANX_Flash', 'enqueue_admin_messages'));
 
 			if ( empty( $_POST ) ) {
 				add_action('admin_init', array($this, 'setup_configs'), 10);
