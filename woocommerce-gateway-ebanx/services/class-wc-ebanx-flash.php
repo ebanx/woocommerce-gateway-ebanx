684825def1371b96dad6b98aa08738b65f9baf1b
Guilherme Pressutto <gpressutto5@gmail.com>
2017-03-29 19:09:46 -0300

    Added phpdoc

--- woocommerce-gateway-ebanx/services/class-wc-ebanx-flash.php
+++ woocommerce-gateway-ebanx/services/class-wc-ebanx-flash.php
@@ -5,8 +5,16 @@ if (!defined('ABSPATH')) {
 }
 
 class WC_EBANX_Flash {
+	/**
+	 * The key we are using on wp_option
+	 */
 	const KEY = '_ebanx_wp_flash_messages';
 
+	/**
+	 * Enqueue every enqueued flash message to the admin_notices hook
+	 *
+	 * @return void
+	 */
 	public static function display_messages() {
 		$flash_messages = self::get_messages();
 		$notices = new WC_EBANX_Notice();
@@ -21,6 +29,14 @@ class WC_EBANX_Flash {
 		}
     }
 
+    /**
+     * Enqueue a message to the next admin_init
+     *
+     * @param  string  $message     The message to enqueue
+     * @param  string  $type        The notice type
+     * @param  boolean $dismissible If the notice will be dismissible
+     * @return void
+     */
     public static function enqueue_message($message, $type = 'error', $dismissible = false) {
     	$flash_messages = maybe_unserialize(get_option(self::KEY, array()));
     	$flash_messages[] = [
@@ -31,6 +47,11 @@ class WC_EBANX_Flash {
     	update_option(self::KEY, $flash_messages);
     }
 
+    /**
+     * Returns all the unqueued flash messages in an array
+     *
+     * @return array All the enqueued flash messages
+     */
     public static function get_messages(){
     	$flash_messages = maybe_unserialize(get_option(self::KEY, array()));
     	delete_option(self::KEY);
