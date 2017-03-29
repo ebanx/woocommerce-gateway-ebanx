289d0b7babd8073d8dcab43695212ed9ba2d33bf
Guilherme Pressutto <gpressutto5@gmail.com>
2017-03-29 15:35:39 -0300

    Validation

--- woocommerce-gateway-ebanx/includes/class-wc-ebanx-payment-by-link.php
+++ woocommerce-gateway-ebanx/includes/class-wc-ebanx-payment-by-link.php
@@ -18,7 +18,9 @@ class WC_EBANX_Payment_By_Link {
 			return;
 		}
 
-		$this->validate();
+		if ( $this->validate() ) {
+			$this->send_errors($this->errors);
+		}
 
 	}
 
@@ -36,20 +38,40 @@ class WC_EBANX_Payment_By_Link {
 	}
 
 	private function validate() {
+		if ( ! $this->order->status === 'pending' ) {
+			$this->errors[] = 'You can only create payment links on pending orders.';
+			return count($this->errors);
+		}
 		if ( $this->order->get_total() < 1 ) {
 			$this->errors[] = 'The total amount needs to be greater than $1.';
-		}
-		if ( $this->order->status === 'pending' ) {
-			$this->errors[] = 'You can only create payment links on pending orders.';
-		}
-		if ( empty($this->order->billing_first_name) || empty($this->order->billing_last_name) ) {
-			$this->errors[] = 'The customer name is required, please provide a valid customer name and last name.';
 		}
 		if ( ! in_array(strtolower($this->order->billing_country), WC_EBANX_Gateway_Utils::$ALL_COUNTRIES) ) {
 			$this->errors[] = 'EBANX only support the countries: Brazil, Mexico, Peru, Colombia and Chile. Please, use one of these.';
 		}
-		if ( ! array_key_exists($this->order->billing_country, WC_EBANX_Gateway_Utils::$GATEWAY_TO_PAYMENT_TYPE_CODE) ) {
-			$this->errors[] = 'EBANX does not support this payment type.';
+		if ( ! array_key_exists($this->order->payment_method, WC_EBANX_Gateway_Utils::$GATEWAY_TO_PAYMENT_TYPE_CODE) ) {
+			$this->errors[] = 'EBANX does not support the selected payment method.';
 		}
+		if ( empty($this->order->billing_email) ) {
+			$this->errors[] = 'The customer e-mal is required, please provide a valid customer e-mail.';
+		}
+		if ( $this->order->payment_method !== ''
+			&& array_key_exists(strtolower($this->order->billing_country), WC_EBANX_Gateway_Utils::$EBANX_GATEWAYS_BY_COUNTRY)
+			&& ! in_array($this->order->payment_method, WC_EBANX_Gateway_Utils::$EBANX_GATEWAYS_BY_COUNTRY[strtolower($this->order->billing_country)]) ) {
+			$this->errors[] = 'The selected payment method is not available on the selected country.';
+		}
+		return count($this->errors);
+	}
+
+	private function send_errors($errors) {
+		//TODO: MAKE IT WORK
+		$this->notices = new WC_EBANX_Notices_Notice();
+			foreach ($errors as $error) {
+				$this->notices
+					->with_message(__($error, 'woocommerce-gateway-ebanx'))
+					->with_type('error')
+					->persistent()
+					->display();
+			}
+			exit;
 	}
 }
