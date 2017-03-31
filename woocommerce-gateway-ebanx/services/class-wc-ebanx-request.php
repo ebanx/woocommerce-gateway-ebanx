1d0680871f58b3cca9e85971d442d5fd881c6963
Guilherme Pressutto <gpressutto5@gmail.com>
2017-03-31 19:17:38 -0300

    Working!

--- woocommerce-gateway-ebanx/services/class-wc-ebanx-request.php
+++ woocommerce-gateway-ebanx/services/class-wc-ebanx-request.php
@@ -8,12 +8,12 @@ class WC_EBANX_Request {
 	const DEFAULT_VALUE = 'WC_EBANX_Request::DEFAULT_VALUE';
 
 	/**
-	 * [read description]
+	 * Reads _REQUEST and gets the value from '$param'
 	 *
-	 * @param  string $param   [description]
-	 * @param  mixed $default [description]
-	 * @return mixed          [description]
-	 * @throws Exception [<description>]
+	 * @param  string $param   The key from _REQUEST
+	 * @param  mixed $default What you want to return if there's no $_REQUEST[$param]
+	 * @return mixed          $_REQUEST[$param] value OR default OR
+	 * @throws Exception Throws exception if there's no $_REQUEST[$param] and no $default
 	 */
 	public static function read($param, $default=self::DEFAULT_VALUE) {
 		if ( self::has($param) ) {
@@ -26,10 +26,10 @@ class WC_EBANX_Request {
 	}
 
 	/**
-	 * [set description]
+	 * Sets $value in $_REQUEST[$param]
 	 *
-	 * @param string $param [description]
-	 * @param mixed $value [description]
+	 * @param string $param The key from _REQUEST
+	 * @param mixed $value The value you want to set
 	 * @return void
 	 */
 	public static function set($param, $value) {
@@ -37,12 +37,12 @@ class WC_EBANX_Request {
 	}
 
 	/**
-	 * [read_values description]
+	 * Reads all keys in array $params from $_REQUEST
 	 *
-	 * @param  string $params  [description]
-	 * @param  mixed $default [description]
-	 * @return mixed          [description]
-	 * @throws Exception [<description>]
+	 * @param  array $params  Array of strings, the values you want to read
+	 * @param  mixed $default What you want to return if there's no value in each $_REQUEST[$params[]]
+	 * @return array          An array with results OR
+	 * @throws Exception Throws exception if there's no $_REQUEST[$param] and no $default
 	 */
 	public static function read_values($params, $default=self::DEFAULT_VALUE) {
 		return array_map(function($param) use ($default) {
@@ -51,19 +51,31 @@ class WC_EBANX_Request {
 	}
 
 	/**
-	 * [has description]
+	 * Check if a key is set on $_REQUEST
 	 *
-	 * @param  string  $param [description]
-	 * @return boolean        [description]
+	 * @param  string  $param The key you want to check
+	 * @return boolean        True if $_REQUEST has $key key
 	 */
-	public static function has($param) {
-		return array_key_exists($param, $_REQUEST);
+	public static function has($key) {
+		return array_key_exists($key, $_REQUEST);
 	}
 
+	/**
+	 * Checks if $_GET is empty.
+	 * Necessary for routers.
+	 *
+	 * @return boolean True if $_GET is empty
+	 */
 	public static function is_get_empty() {
 		return empty($_GET);
 	}
 
+	/**
+	 * Checks if $_POST is empty.
+	 * Necessary for routers.
+	 *
+	 * @return boolean True if $_POST is empty
+	 */
 	public static function is_post_empty() {
 		return empty($_POST);
 	}
