8a1eb3a283d014883734bd9f6a14aa0eda38a00d
Guilherme Pressutto <gpressutto5@gmail.com>
2017-04-04 16:19:13 -0300

    Changed get_country to get_billing_country

--- woocommerce-gateway-ebanx/gateways/class-wc-ebanx-gateway.php
+++ woocommerce-gateway-ebanx/gateways/class-wc-ebanx-gateway.php
@@ -615,14 +615,14 @@ abstract class WC_EBANX_Gateway extends WC_Payment_Gateway
 	 */
 	protected function getTransactionAddress($attr = '')
 	{
-		if (empty(WC()->customer) || is_admin() || (empty($_POST['billing_country']) && empty(WC()->customer->get_country()))) {
+		if (empty(WC()->customer) || is_admin() || (empty($_POST['billing_country']) && empty(WC()->customer->get_billing_country()))) {
 			return false;
 		}
 
 		if (!empty($_POST['billing_country'])) {
 			$this->address['country'] = trim(strtolower($_POST['billing_country']));
 		} else {
-			$this->address['country'] = trim(strtolower(WC()->customer->get_country()));
+			$this->address['country'] = trim(strtolower(WC()->customer->get_billing_country()));
 		}
 
 		if ($attr !== '' && !empty($this->address[$attr])) {
@@ -661,7 +661,7 @@ abstract class WC_EBANX_Gateway extends WC_Payment_Gateway
 				$request = \Ebanx\EBANX::doRequest($data);
 
 				if (
-					$request->payment->transaction_status->code === 'NOK' 
+					$request->payment->transaction_status->code === 'NOK'
 					&& $request->payment->transaction_status->acquirer === 'EBANX'
 					&& $this->is_sandbox_mode
 				) {
