ddb92a49e839e16a1258a4f760419ed27c2e0dd4
Anderson Campanha <anderson.campanha@ebanx.com>
2017-07-20 13:34:47 -0300

    Minor style changes

--- woocommerce-gateway-ebanx/gateways/class-wc-ebanx-credit-card-gateway.php
+++ woocommerce-gateway-ebanx/gateways/class-wc-ebanx-credit-card-gateway.php
@@ -352,21 +352,24 @@ abstract class WC_EBANX_Credit_Card_Gateway extends WC_EBANX_Gateway
 				$merchant_min_instalment_value = $this->get_setting_or_default("min_instalment_value_$currency_code", 0) * $site_to_local_rate;
 				$min_instalment_value = max(
 					WC_EBANX_Constants::ACQUIRER_MIN_INSTALMENT_VALUE_BRL,
-					$merchant_min_instalment_value);
+					$merchant_min_instalment_value
+				);
 				break;
 			case 'mx':
 				$site_to_local_rate = $this->get_local_currency_rate_for_site(WC_EBANX_Constants::CURRENCY_CODE_MXN);
 				$merchant_min_instalment_value = $this->get_setting_or_default("min_instalment_value_$currency_code", 0) * $site_to_local_rate;
 				$min_instalment_value = max(
 					WC_EBANX_Constants::ACQUIRER_MIN_INSTALMENT_VALUE_MXN,
-					$merchant_min_instalment_value);
+					$merchant_min_instalment_value
+				);
 				break;
 			case 'co':
 				$site_to_local_rate = $this->get_local_currency_rate_for_site(WC_EBANX_Constants::CURRENCY_CODE_COP);
 				$merchant_min_instalment_value = $this->get_setting_or_default("min_instalment_value_$currency_code", 0) * $site_to_local_rate;
 				$min_instalment_value = max(
 					WC_EBANX_Constants::ACQUIRER_MIN_INSTALMENT_VALUE_COP,
-					$merchant_min_instalment_value);
+					$merchant_min_instalment_value
+				);
 				break;
 		}
 
