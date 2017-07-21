5c0bccb4c61a804306fe9fb92a6839caf5de0b92
Cezar Luiz <cezarluiz.c@gmail.com>
2017-07-21 18:37:49 -0300

    Updated credit card thank you page for Mexico

--- woocommerce-gateway-ebanx/templates/ebanx-credit-card-mx/payment-processing.php
+++ woocommerce-gateway-ebanx/templates/ebanx-credit-card-mx/payment-processing.php
@@ -12,13 +12,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-<p><strong><?php _e(sprintf('Pago aprobado con éxito, %s.', $customer_name), 'woocommerce-gateway-ebanx') ?></strong></p>
-<p><strong><?php _e('Resumo de la compra:', 'woocommerce-gateway-ebanx') ?></strong></p>
-<p><?php _e('Valor:', 'woocommerce-gateway-ebanx') ?> <?php echo $total ?></p>
 <?php if ($instalments_number > 1) : ?>
-    <p><?php echo $instalments_number ?> <?php _e('meses sen intereses de', 'woocommerce-gateway-ebanx') ?> <?php echo $instalments_amount ?></p>
-<?php else : ?>
-    <p><?php _e('Pago realizado en una sola exhibición', 'woocommerce-gateway-ebanx') ?></p>
-<?php endif; ?>
-<p><?php _e(sprintf('Pago con tarjeta %s:', ucwords($card_brand_name)), 'woocommerce-gateway-ebanx') ?> <?php echo $masked_card ?></p>
-<p><?php _e('Gracias por haber comprado con nosotros.', 'woocommerce-gateway-ebanx') ?></p>
+	<p><strong><?= $customer_name ?> tu pago de <?= $total ?>, dividido en <?= $instalments_number ?> meses de <?= $instalments_amount ?>, fue aprobado</strong></p>
+<?php else: ?>
+	<p><strong><?= $customer_name ?> tu pago de <?= $total ?>, en una sola exibición, fue aprobado o/</strong></p>
+<?php endif ?>
+
+<p>Se tienes alguna duda en relación a tu pago, ingresa a la Cuenta EBANX con el email <?= $customer_email ?></p>
