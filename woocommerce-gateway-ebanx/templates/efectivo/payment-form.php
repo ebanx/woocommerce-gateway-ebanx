1401fa72d942414871e24eff91c097daf379bf12
Guilherme Pressutto <gpressutto5@gmail.com>
2017-10-03 14:18:51 -0300

    Added images to efectivo form

--- woocommerce-gateway-ebanx/templates/efectivo/payment-form.php
+++ woocommerce-gateway-ebanx/templates/efectivo/payment-form.php
@@ -16,17 +16,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<div class="ebanx-form-row">
 		<label class="ebanx-label">
-			<input type="radio" name="efectivo" value="rapipago" checked> Rapipago
+			<input type="radio" name="efectivo" value="rapipago" checked> Rapipago <img src="<?php echo WC_EBANX_PLUGIN_DIR_URL ?>assets/images/ebanx-rapipago.png" height="20" />
 		</label>
 	</div>
 	<div class="ebanx-form-row">
 		<label class="ebanx-label">
-			<input type="radio" name="efectivo" value="pagofacil"> Pagofacil
+			<input type="radio" name="efectivo" value="pagofacil"> Pagofacil <img src="<?php echo WC_EBANX_PLUGIN_DIR_URL ?>assets/images/ebanx-pagofacil.png" height="20" />
 		</label>
 	</div>
 	<div class="ebanx-form-row">
 		<label class="ebanx-label">
-			<input type="radio" name="efectivo" value="cupon"> Otros Cupones
+			<input type="radio" name="efectivo" value="cupon"> Otros Cupones <img src="<?php echo WC_EBANX_PLUGIN_DIR_URL ?>assets/images/ebanx-cupon.png" height="20" />
 		</label>
 	</div>
 </div>
