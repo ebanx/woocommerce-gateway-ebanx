422c5af9dc346ca75c8e6ae0cc523719911f46fd
Guilherme Pressutto <gpressutto5@gmail.com>
2017-10-03 16:12:50 -0300

    Using images on the left on Efectivo

--- woocommerce-gateway-ebanx/templates/efectivo/payment-form.php
+++ woocommerce-gateway-ebanx/templates/efectivo/payment-form.php
@@ -16,17 +16,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<div class="ebanx-form-row">
 		<label class="ebanx-label">
-			<input type="radio" name="efectivo" value="rapipago" checked> Rapipago <img src="<?php echo WC_EBANX_PLUGIN_DIR_URL ?>assets/images/ebanx-rapipago.png" height="20" />
+			<input type="radio" name="efectivo" value="rapipago" checked> <img src="<?php echo WC_EBANX_PLUGIN_DIR_URL ?>assets/images/ebanx-rapipago.png" style="height: 20px" /> Rapipago
 		</label>
 	</div>
 	<div class="ebanx-form-row">
 		<label class="ebanx-label">
-			<input type="radio" name="efectivo" value="pagofacil"> Pagofacil <img src="<?php echo WC_EBANX_PLUGIN_DIR_URL ?>assets/images/ebanx-pagofacil.png" height="20" />
+			<input type="radio" name="efectivo" value="pagofacil"> <img src="<?php echo WC_EBANX_PLUGIN_DIR_URL ?>assets/images/ebanx-pagofacil.png" style="height: 20px" /> Pagofacil
 		</label>
 	</div>
 	<div class="ebanx-form-row">
 		<label class="ebanx-label">
-			<input type="radio" name="efectivo" value="cupon"> Otros Cupones <img src="<?php echo WC_EBANX_PLUGIN_DIR_URL ?>assets/images/ebanx-cupon.png" height="20" />
+			<input type="radio" name="efectivo" value="cupon"> <img src="<?php echo WC_EBANX_PLUGIN_DIR_URL ?>assets/images/ebanx-cupon.png" style="height: 20px" /> Otros Cupones
 		</label>
 	</div>
 </div>
