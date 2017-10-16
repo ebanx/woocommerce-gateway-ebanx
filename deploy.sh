36ef400811d800ee8c50954356fd8149f2abc2b6
Guilherme Pressutto <gpressutto5@gmail.com>
2017-10-16 19:20:33 -0200

    Fixed travis branch requirements

--- deploy.sh
+++ deploy.sh
@@ -5,88 +5,72 @@ if [[ -z "$TRAVIS" ]]; then
 	exit 1
 fi
 
-if [[ -z "$WP_ORG_PASSWORD" ]]; then
-	echo "WordPress.org password not set" 1>&2
-	exit 1
-fi
-
-if [[ -z "$TRAVIS_BRANCH" || "$TRAVIS_BRANCH" != "master" ]]; then
-	echo "Build branch is required and must be 'master'" 1>&2
-	exit 0
-fi
-
-WP_ORG_USERNAME="ebanxwp"
-PLUGIN="woocommerce-gateway-ebanx"
-SLUG="ebanx-payment-gateway-for-woocommerce"
-PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-PLUGIN_BUILDS_PATH="$PROJECT_ROOT/build"
-VERSION=$TRAVIS_TAG
-ZIP_FILE="$PROJECT_ROOT/$PLUGIN.zip"
-
-# Ensure the zip file for the current version has been built
-if [ ! -f "$ZIP_FILE" ]; then
-    echo "Built zip file $ZIP_FILE does not exist" 1>&2
+if [[ -z "$WP_ORG_USERNAME" ]]; then
+    echo "WordPress.org username not set" 1>&2
     exit 1
 fi
 
-cd "$PLUGIN_BUILDS_PATH"
-cp "$ZIP_FILE" "$PLUGIN_BUILDS_PATH/$PLUGIN.zip"
-ZIP_FILE="$PLUGIN_BUILDS_PATH/$PLUGIN.zip"
-# Remove any unzipped dir so we start from scratch
-rm -fR "$PLUGIN"
-# Unzip the built plugin
-unzip -q -o "$ZIP_FILE"
+if [[ -z "$WP_ORG_PASSWORD" ]]; then
+    echo "WordPress.org password not set" 1>&2
+    exit 1
+fi
+
+if [[ -z "$TRAVIS_TAG" ]]; then
+    echo "Build must be tag" 1>&2
+    exit 0
+fi
+
+PLUGIN="woocommerce-gateway-ebanx"
+SLUG="ebanx-payment-gateway-for-woocommerce"
+TRAVIS_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+PROJECT_ROOT="$TRAVIS_ROOT/$PLUGIN"
+PLUGIN_BUILDS_PATH="$PROJECT_ROOT/build"
+SVN_ROOT_PATH="$PLUGIN_BUILDS_PATH/svn"
+VERSION=$TRAVIS_TAG
+ZIP_FILE="$PROJECT_ROOT/$SLUG.zip"
+
+# Ensure the zip file for the current version has been built
+if [ -f "$ZIP_FILE" ]; then
+    echo "Found zip file $ZIP_FILE ... discarding."
+    rm -f $ZIP_FILE
+fi
 
 # Clean up any previous svn dir
-rm -fR svn
+rm -fR $SVN_ROOT_PATH
 
+echo "Performing Checkout from http://svn.wp-plugins.org/$SLUG"
 # Checkout the SVN repo
-svn co -q "http://svn.wp-plugins.org/$SLUG" svn
+svn co -q "http://svn.wp-plugins.org/$SLUG" $SVN_ROOT_PATH
 
-# Move out the trunk directory to a temp location
-mv svn/trunk ./svn-trunk
+# Erase trunk
+rm -fR "$SVN_ROOT_PATH/trunk"
 # Create trunk directory
-mkdir svn/trunk
+mkdir $SVN_ROOT_PATH/trunk
+
+echo "Synchronizing trunk"
 # Copy our new version of the plugin into trunk
-rsync -r -p $PLUGIN/* svn/trunk
+rsync -r -p --exclude "build" $PROJECT_ROOT/* $SVN_ROOT_PATH/trunk
 
-# Copy all the .svn folders from the checked out copy of trunk to the new trunk.
-# This is necessary as the Travis container runs Subversion 1.6 which has .svn dirs in every sub dir
-cd svn/trunk/
-TARGET=$(pwd)
-cd ../../svn-trunk/
-
-# Find all .svn dirs in sub dirs
-SVN_DIRS=`find . -type d -iname .svn`
-
-for SVN_DIR in $SVN_DIRS; do
-    SOURCE_DIR=${SVN_DIR/.}
-    TARGET_DIR=$TARGET${SOURCE_DIR/.svn}
-    TARGET_SVN_DIR=$TARGET${SVN_DIR/.}
-    if [ -d "$TARGET_DIR" ]; then
-        # Copy the .svn directory to trunk dir
-        cp -r $SVN_DIR $TARGET_SVN_DIR
-    fi
-done
-
-# Back to builds dir
-cd ../
-
-# Remove checked out dir
-rm -fR svn-trunk
-
+# Erase possible broken version
+rm -fR "$SVN_ROOT_PATH/tags/$VERSION"
 # Add new version tag
-mkdir svn/tags/$VERSION
-rsync -r -p $PLUGIN/* svn/tags/$VERSION
+mkdir $SVN_ROOT_PATH/tags/$VERSION
 
+echo "Synchronizing tag $VERSION"
+# Copy our new version of the plugin into tag directory
+rsync -r -p --exclude "build" $PROJECT_ROOT/* $SVN_ROOT_PATH/tags/$VERSION
+
+echo "Generating diff"
 # Add new files to SVN
-svn stat svn | grep '^?' | awk '{print $2}' | xargs -I x svn add x@
+svn stat $SVN_ROOT_PATH | grep '^?' | awk '{print $2}' | xargs -I x svn add x@
 # Remove deleted files from SVN
-svn stat svn | grep '^!' | awk '{print $2}' | xargs -I x svn rm --force x@
-svn stat svn
+svn stat $SVN_ROOT_PATH | grep '^!' | awk '{print $2}' | xargs -I x svn rm --force x@
+svn stat $SVN_ROOT_PATH
 
+echo "Uploading to SVN"
 # Commit to SVN
-svn ci --no-auth-cache --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD svn -m "Deploy version $VERSION"
+svn ci --no-auth-cache --username $WP_ORG_USERNAME --password $WP_ORG_PASSWORD $SVN_ROOT_PATH -m "Deploy version $VERSION"
 
+echo "Cleaning temp dirs"
 # Remove SVN temp dir
-rm -fR svn
+rm -fR $SVN_ROOT_PATH
