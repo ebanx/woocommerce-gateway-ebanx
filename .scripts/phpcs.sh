9fce7e71b3f6a35542a1ab6eed38ae73ba01118b
Guilherme Pressutto <gpressutto5@gmail.com>
2018-02-08 15:13:18 -0200

    Checking style of added lines only

--- .scripts/phpcs.sh
+++ .scripts/phpcs.sh
@@ -1,8 +1,29 @@
 #!/usr/bin/env bash
 
-CHANGED_FILES=`git diff --name-only --diff-filter=ACMR $TRAVIS_COMMIT_RANGE | grep \\\\.php | awk '{print}' ORS=' '`
+# we need this script to fail if any command fails
+# when a command fails the variable `err` will turn 1
+err=0
+trap 'err=1' ERR
 
-if [ "$CHANGED_FILES" != "" ]; then
-    echo "Running Code Sniffer."
-    ./vendor/bin/phpcs --encoding=utf-8 -n -p $CHANGED_FILES
-fi
+# ensure that files do not exist
+# -f makes sure it returns 0 if the files didn't exist
+rm -f diff.txt phpcs.json
+
+# get diff from last travis build
+# if PR, get diff from the base branch
+git diff $TRAVIS_COMMIT_RANGE > diff.txt
+
+# get all the style errors
+# `|| true` makes sure it returns 0 even when phpcs fails
+./vendor/bin/phpcs ./ --report=json --report-file=phpcs.json || true
+
+# makes sure no added line has error
+./vendor/bin/diffFilter --phpcs diff.txt phpcs.json
+
+# remove files created only for this scripts
+# -f makes sure it returns 0 if the files didn't exist
+rm -f diff.txt phpcs.json
+
+# 0 if no command failed
+# 1 if a command failed
+exit ${err}
