a134ff6973ac7921a6c6ece1731769fc466f6b88
Anderson Campanha <anderson.campanha@ebanx.com>
2018-02-16 12:06:03 -0200

    Throw exception if DNI is not present for cc

--- gateways/class-wc-ebanx-gateway.php
+++ gateways/class-wc-ebanx-gateway.php
@@ -602,6 +602,10 @@ class WC_EBANX_Gateway extends WC_Payment_Gateway
 		}
 
 		if ($this->getTransactionAddress('country') === WC_EBANX_Constants::COUNTRY_COLOMBIA) {
+			if ( empty($payload['ebanx_billing_colombia_document']) && $order->get_payment_method() === 'ebanx-credit-card-co') {
+				throw new Exception('BP-DR-22');
+			}
+
 			WC_EBANX_Request::set('ebanx_billing_document', $payload['ebanx_billing_colombia_document']);
 		}
 
