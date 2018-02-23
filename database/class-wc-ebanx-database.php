1837adde0b9eb24d0a66f8224cc8ba634c7c9402
Anderson Campanha <anderson.campanha@ebanx.com>
2018-02-23 09:42:17 -0300

    Code style changes

--- database/class-wc-ebanx-database.php
+++ database/class-wc-ebanx-database.php
@@ -2,14 +2,20 @@
 
 require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 
+/**
+ * Class WC_EBANX_Database
+ */
 class WC_EBANX_Database {
 
+	/**
+	 * Cretaes EBANX logs table on database.
+	 */
 	public function create_log_table() {
 		global $wpdb;
 		$table_name = $wpdb->prefix . 'ebanx_logs';
 		$charset_collate = $wpdb->get_charset_collate();
 
-		if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") === $table_name) {
+		if ( $wpdb->get_var( $wpdb->prepare( "SHOW TABLES LIKE '%s'", $table_name ) ) ) {
 			return;
 		}
 
