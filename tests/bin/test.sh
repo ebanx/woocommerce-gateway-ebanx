9ba6e9f2fdba261ae4fe47e40c0026954e8ffcd1
Cristopher Rodrigues <cristopher.rodrigues993@gmail.com>
2018-02-26 12:55:28 -0300

    Sleep again

--- tests/bin/test.sh
+++ tests/bin/test.sh
@@ -3,6 +3,7 @@
 [[ $TRAVIS_COMMIT_MESSAGE =~ ^(\[tests skip\]) ]] && echo "TESTS SKIP" && exit 0;
 
 setup_test() {
+  echo setup_test
   cd $TRAVIS_BUILD_DIR/tests
   npm install
 
@@ -15,11 +16,13 @@ setup_test() {
 }
 
 run_tests() {
+  echo run_tests
   cd $TRAVIS_BUILD_DIR/tests
   node ./node_modules/.bin/cypress run --config videoRecording=false --project ./woocommerce -s cypress/integration/$TEST_COUNTRY.js
 }
 
 setup_docker() {
+  echo setup_docker
   cd $TRAVIS_BUILD_DIR
   docker-compose up -d --build
 }
@@ -27,4 +30,4 @@ setup_docker() {
 setup_test
 setup_docker
 
-while ! curl -s http://localhost > /dev/null; do echo waiting for woocommerce-container; sleep 3; done; run_tests
+while ! curl -s http://localhost > /dev/null; do echo waiting for woocommerce-container; sleep 10; done; run_tests
