cb4cc5d8aa848dd2285b2e1040a732a5e7cc222a
Cristopher Rodrigues <cristopher.rodrigues993@gmail.com>
2018-02-26 09:29:26 -0300

    Try another approach to run tests

--- tests/bin/test.sh
+++ tests/bin/test.sh
@@ -2,12 +2,28 @@
 
 [[ $TRAVIS_COMMIT_MESSAGE =~ ^(\[tests skip\]) ]] && echo "TESTS SKIP" && exit 0;
 
-cd $TRAVIS_BUILD_DIR
+run_test() {
+  cd $TRAVIS_BUILD_DIR/tests
 
-docker-compose up -d
-sleep 120 # REMOVE THAT
+  ls
 
-cd $TRAVIS_BUILD_DIR/tests
+  npm install
+  node ./node_modules/.bin/cypress run --config videoRecording=false --project ./woocommerce -s cypress/integration/$TEST_COUNTRY.js
+}
 
-npm install
-node ./node_modules/.bin/cypress run --config videoRecording=false --project ./woocommerce -s cypress/integration/$TEST_COUNTRY.js
+setup_docker() {
+  cd $TRAVIS_BUILD_DIR
+  docker-compose up -d --build
+  sleep 20 # REMOVE THAT
+}
+
+setup_travis() {
+  sudo service mysql stop
+  "export DISPLAY=:99.0"
+  "sh -e /etc/init.d/xvfb start"
+  sleep 3
+}
+
+setup_travis
+setup_docker
+run_test
