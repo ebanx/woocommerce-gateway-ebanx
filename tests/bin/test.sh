3c324e2d621043c7994b287fc57f8b5fd3d2214c
Cristopher Rodrigues <cristopher.rodrigues993@gmail.com>
2018-02-26 16:00:35 -0300

    Try run before setup

--- tests/bin/test.sh
+++ tests/bin/test.sh
@@ -7,17 +7,15 @@ setup_test() {
   cd $TRAVIS_BUILD_DIR/tests
   npm install
   ./node_modules/.bin/cypress -v
-  ./node_modules/.bin/cypress run --config videoRecording=false --project ./woocommerce -s cypress/integration/$TEST_COUNTRY.js
-  exit 1;
   sudo service mysql stop
   "export DISPLAY=:99.0"
   "sh -e /etc/init.d/xvfb start"
 }
 
 run_tests() {
+  setup_test
   echo run_tests
   cd $TRAVIS_BUILD_DIR/tests
-  sudo chmod +x ./node_modules/.bin/cypress
   ./node_modules/.bin/cypress run --config videoRecording=false --project ./woocommerce -s cypress/integration/$TEST_COUNTRY.js
 }
 
@@ -27,7 +25,6 @@ setup_docker() {
   docker-compose up -d --build
 }
 
-setup_test
 setup_docker
 
 while ! curl -s http://localhost > /dev/null; do echo waiting for woocommerce-container; sleep 10; done; run_tests
