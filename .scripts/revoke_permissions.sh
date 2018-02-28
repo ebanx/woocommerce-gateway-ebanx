382c8e956e6d4a5a699b8555a49543fb69a1e27d
Cristopher Rodrigues <cristopher.rodrigues993@gmail.com>
2018-02-28 16:04:27 -0300

    Revoke phpcs

--- .scripts/revoke_permissions.sh
+++ .scripts/revoke_permissions.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+sudo chown -R travis:travis $TRAVIS_BUILD_DIR/vendor
 sudo chmod -R 777 $TRAVIS_BUILD_DIR/vendor
+chmod +x $TRAVIS_BUILD_DIR/vendor/bin/phpcs
+chmod +x $TRAVIS_BUILD_DIR/vendor/bin/phpunit
 sudo chown -R travis:travis $TRAVIS_BUILD_DIR/tests
