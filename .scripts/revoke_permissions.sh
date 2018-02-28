5e04bd9632d085b757806be0c903e839a8d46ee5
Cristopher Rodrigues <cristopher.rodrigues993@gmail.com>
2018-02-28 14:50:24 -0300

    Revoke to node modules

--- .scripts/revoke_permissions.sh
+++ .scripts/revoke_permissions.sh
@@ -2,3 +2,6 @@
 
 sudo chmod -R 777 $TRAVIS_BUILD_DIR/vendor
 sudo chmod -R 777 $TRAVIS_BUILD_DIR/tests/
+
+chown -R travis:travis $TRAVIS_BUILD_DIR/tests/node_modules
+sudo chmod -R 777 $TRAVIS_BUILD_DIR/tests/node_modules
