f38ac74c9290a4805484f94c13e3d44b712fb3e2
Giulia <gcesarpadovani@hotmail.com>
2018-04-06 14:27:05 -0300

    ebanx-credit-card-mx/payment-processing.php

--- templates/ebanx-credit-card-mx/payment-processing.php
+++ templates/ebanx-credit-card-mx/payment-processing.php
@@ -16,9 +16,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<?php if ( $instalments_number > 1 ) : ?>
 		<p><strong><?php echo esc_html( $customer_name ); ?> tu pago de <?php echo esc_html( $total ); ?>, dividido en <span id="ebanx-instalment-number"><?php echo esc_html( $instalments_number ); ?> meses de <?php echo esc_html( $instalments_amount ); ?>, fue aprobado</strong></p>
 	<?php else : ?>
-		<p><strong><?php echo $customer_name; ?> tu pago de <?php echo $total; ?>, en una sola exibición, fue aprobado o/</strong></p>
+		<p><strong><?php echo esc_html( $customer_name ); ?> tu pago de <?php echo esc_html( $total ); ?>, en una sola exibición, fue aprobado o/</strong></p>
 	<?php endif ?>
 
-	<p>Se tienes alguna duda en relación a tu pago, ingresa a la Cuenta EBANX con el email <?php echo $customer_email; ?></p>
+	<p>Se tienes alguna duda en relación a tu pago, ingresa a la Cuenta EBANX con el email <?php echo esc_html( $customer_email ); ?></p>
 	<input type="hidden" id="ebanx-payment-hash" data-doraemon-hash="<?php echo esc_html( $hash ); ?>">
 </div>
