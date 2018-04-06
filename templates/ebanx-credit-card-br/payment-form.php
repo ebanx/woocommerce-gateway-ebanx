6c341e987dcc4b3e5c7479eb749c663d4e2f466d
Giulia <gcesarpadovani@hotmail.com>
2018-04-06 14:42:15 -0300

    ebanx-credit-card-br/payment-form.php

--- templates/ebanx-credit-card-br/payment-form.php
+++ templates/ebanx-credit-card-br/payment-form.php
@@ -22,18 +22,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 					<label class="ebanx-credit-card-label">
 						<input type="radio" 
 						<?php
-						if ( $k === 0 ) :
+						if ( 0 === $k ) :
 ?>
-checked="checked"<?php endif; ?> class="input-radio <?php echo trim( $card->brand . '-' . $card->masked_number ); ?>" value="<?php echo $card->token; ?>" name="ebanx-credit-card-use" />
+checked="checked"<?php endif; ?> class="input-radio <?php echo esc_attr( trim( $card->brand . '-' . $card->masked_number ) ); ?>" value="<?php echo esc_attr( $card->token ); ?>" name="ebanx-credit-card-use" />
 						<span class="ebanx-credit-card-brand">
-							<img src="<?php echo WC_EBANX_PLUGIN_DIR_URL . "assets/images/icons/$card->brand.png"; ?>" height="20" style="height: 20px; margin-left: 0; margin-right: 7px; float: none;" alt="<?php echo $card->brand; ?>">
+							<img src="<?php echo esc_url( WC_EBANX_PLUGIN_DIR_URL . "assets/images/icons/$card->brand.png" ); ?>" height="20" style="height: 20px; margin-left: 0; margin-right: 7px; float: none;" alt="<?php echo esc_attr( $card->brand ); ?>">
 						</span>
-						<span class="ebanx-credit-card-bin">&bull;&bull;&bull;&bull; <?php echo substr( $card->masked_number, -4 ); ?></span>
+						<span class="ebanx-credit-card-bin">&bull;&bull;&bull;&bull; <?php echo esc_html( substr( $card->masked_number, -4 ) ); ?></span>
 					</label>
 					<div class="clear"></div>
 					<div class="ebanx-container-credit-card" style="
 					<?php
-					if ( $k !== 0 ) :
+					if ( 0 !== $k ) :
 ?>
 display: none;<?php endif; ?>">
 						<section class="ebanx-form-row">
@@ -41,8 +41,8 @@ display: none;<?php endif; ?>">
 								<label for="ebanx-card-cvv">Código de segurança<span class="required">*</span></label>
 
 								<input class="input-text wc-credit-card-form-card-cvc" type="text" autocomplete="off" placeholder="CVV" style="float: none;" />
-								<input type="hidden" autocomplete="off" value="<?php echo $card->brand; ?>" class="ebanx-card-brand-use" />
-								<input type="hidden" autocomplete="off" value="<?php echo $card->masked_number; ?>" class="ebanx-card-masked-number-use" />
+								<input type="hidden" autocomplete="off" value="<?php echo esc_attr( $card->brand ); ?>" class="ebanx-card-brand-use" />
+								<input type="hidden" autocomplete="off" value="<?php echo esc_attr( $card->masked_number ); ?>" class="ebanx-card-masked-number-use" />
 							</section>
 
 							<?php include WC_EBANX::get_templates_path() . 'instalments.php'; ?>
