0cfebc4c716a026edd100c5cf43c8e79b72f20c2
Pablo Suchewschy <pblwlln@gmail.com>
2018-04-09 17:00:38 -0300

    Lint parallels

--- .scripts/phpcs.sh
+++ .scripts/phpcs.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-# we need this script to fail if any command fails
-# when a command fails the variable `err` will turn 1
-err=0
-trap 'err=1' ERR
+php $(pwd)/vendor/squizlabs/php_codesniffer/bin/phpcs --extensions=php,phtml -v -n --parallel=5 .
 
-# get all the style errors
-# `|| true` makes sure it returns 0 even when phpcs fails
-./vendor/bin/phpcs ./ || true
+# # we need this script to fail if any command fails
+# # when a command fails the variable `err` will turn 1
+# err=0
+# trap 'err=1' ERR
 
-# 0 if no command failed
-# 1 if a command failed
-exit ${err}
+# # get all the style errors
+# # `|| true` makes sure it returns 0 even when phpcs fails
+# ./vendor/bin/phpcs ./ || true
+
+# # 0 if no command failed
+# # 1 if a command failed
+# exit ${err}
