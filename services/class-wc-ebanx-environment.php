0acbd5d45c1dec9c26872fe056e2c58eaccead4f
Zoch, Romulo <Romulo.Zoch@mastercard.com>
2018-07-23 03:11:23 -0300

    sanitize $_SERVER usage

--- services/class-wc-ebanx-environment.php
+++ services/class-wc-ebanx-environment.php
@@ -73,7 +73,7 @@ class WC_EBANX_Environment {
 			$web_server->version           = $web_server_parts[1];
 		} else {
 			$web_server                    = new stdClass();
-			$web_server->name              = $_SERVER['SERVER_NAME'];
+			$web_server->name              = sanitize_text_field( wp_unslash( $_SERVER['SERVER_NAME'] ) );
 			$web_server->version           = PHP_SAPI;
 		}
 
