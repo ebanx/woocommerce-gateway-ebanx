112c79aba61a402a725ed1b59f599a65e71e3345
Zoch, Romulo <Romulo.Zoch@mastercard.com>
2018-07-23 03:48:08 -0300

    validate $_SERVER existance for linter

--- services/class-wc-ebanx-environment.php
+++ services/class-wc-ebanx-environment.php
@@ -72,9 +72,11 @@ class WC_EBANX_Environment {
 			$web_server->name              = str_replace( '-', ' ', $web_server_parts[0] );
 			$web_server->version           = $web_server_parts[1];
 		} else {
-			$web_server                    = new stdClass();
-			$web_server->name              = sanitize_text_field( wp_unslash( $_SERVER['SERVER_NAME'] ) );
-			$web_server->version           = PHP_SAPI;
+			if ( isset($_SERVER) && isset($_SERVER['SERVER_NAME']) ) {
+				$web_server                    = new stdClass();
+				$web_server->name              = sanitize_text_field( wp_unslash( $_SERVER['SERVER_NAME'] ) );
+				$web_server->version           = PHP_SAPI;
+			}
 		}
 
 		$this->web_server = $web_server;
