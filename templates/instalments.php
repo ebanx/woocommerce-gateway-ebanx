ade54096aff063855e6eb716befd49cb8bef0169
Anderson Campanha <anderson.campanha@ebanx.com>
2018-10-03 18:15:19 -0300

    Trim spaces when printing value

--- templates/instalments.php
+++ templates/instalments.php
@@ -23,12 +23,10 @@ if ( count( $instalments_terms ) > 1 ) : ?>
 				name="ebanx-credit-card-installments"
 			>
 				<?php foreach ( $instalments_terms as $instalment ) : ?>
-					<option value="
-					<?php
+					<option value="<?php // @codingStandardsIgnoreLine
 							// phpcs:ignore WordPress.NamingConventions.ValidVariableName
-							echo esc_attr( $instalment->instalmentNumber );
-					?>
-					">
+							echo esc_attr( trim( $instalment->instalmentNumber ) );
+					// @codingStandardsIgnoreLine?>">
 						<?php
 							// @codingStandardsIgnoreLine
 							printf( __( '%1$dx of %2$s', 'woocommerce-gateway-ebanx' ), absint( $instalment->instalmentNumber ), esc_html( strip_tags( wc_price( $instalment->localAmountWithTax, array( 'currency' => $currency ) ) ) ) );
