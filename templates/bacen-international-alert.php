6e37ac7fc65d9c46fbfb3eac11bf0694c362c4e3
João Batista Neto <neto.joaobatista@gmail.com>
2021-10-05 10:15:00 -0300

    Improve bacen message

--- templates/bacen-international-alert.php
+++ templates/bacen-international-alert.php
@@ -5,6 +5,6 @@ if ( ! $is_international ) {
 ?>
 <div class="international-alert-box" style="margin-top: 1em;">
 	<small class="international-alert-message">
-		Esta é uma compra internacional, mediante operação de câmbio, que será realizada pela EBANX de acordo com seus <strong><a href="https://www.ebanx.com/br/termos/">termos e condições</a></strong>.
+		Esta é uma compra internacional, mediante operação de câmbio, que será intermediada pelo EBANX de acordo com seus <strong><a href="https://www.ebanx.com/br/termos/">termos e condições</a></strong>. Ao clicar em pagar, você dá ciência e aceita os termos desta transação.
 	</small>
 </div>
