b0a382767bc2ddde50f3c9b5d99efc8104ba8c19
João Batista Neto <neto.joaobatista@gmail.com>
2022-01-18 10:33:39 -0300

    Update services/loggers/class-wc-ebanx-subscription-renewal-logger.php
    Co-authored-by: Ale Bark Bruneri <ale.bark_bruneri@hotmail.com>

--- services/loggers/class-wc-ebanx-subscription-renewal-logger.php
+++ services/loggers/class-wc-ebanx-subscription-renewal-logger.php
@@ -9,7 +9,7 @@ final class WC_EBANX_Subscription_Renewal_Logger extends WC_EBANX_Logger {
 	 * @param array $log_data data to be logged.
 	 * @param string $event event name to be logged.
 	 */
-	public static function persist( array $log_data = [], $event = null) {
-		parent::save( 'subscription_renewal', $log_data );
+	public static function persist(array $log_data = [], $event = 'subscription_renewal') {
+		parent::save($event, $log_data);
 	}
 }
