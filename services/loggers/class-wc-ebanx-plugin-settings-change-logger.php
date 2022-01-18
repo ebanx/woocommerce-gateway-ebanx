6bc76dabb555d6d867db6c770ec7fc85e080b5e1
João Batista Neto <neto.joaobatista@gmail.com>
2022-01-18 10:34:01 -0300

    Update services/loggers/class-wc-ebanx-plugin-settings-change-logger.php
    Co-authored-by: Ale Bark Bruneri <ale.bark_bruneri@hotmail.com>

--- services/loggers/class-wc-ebanx-plugin-settings-change-logger.php
+++ services/loggers/class-wc-ebanx-plugin-settings-change-logger.php
@@ -11,9 +11,9 @@ final class WC_EBANX_Plugin_Settings_Change_Logger extends WC_EBANX_Logger {
 	 * @param array $log_data data to be logged.
 	 * @param string $event event name to be logged.
 	 */
-	public static function persist( array $log_data = [], $event = null) {
+	public static function persist(array $log_data = [], $event = 'plugin_settings_change') {
 		parent::save(
-			'plugin_settings_change',
+			$event,
 			array_merge(
 				WC_EBANX_Log::get_platform_info(),
 				[ 'settings' => $log_data ]
