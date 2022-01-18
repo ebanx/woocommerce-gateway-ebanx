3bbfb0f4d2f0c675740618e81dd884ff270d73fa
João Batista Neto <neto.joaobatista@gmail.com>
2022-01-18 10:34:25 -0300

    Update services/loggers/class-wc-ebanx-plugin-activate-logger.php
    Co-authored-by: Ale Bark Bruneri <ale.bark_bruneri@hotmail.com>

--- services/loggers/class-wc-ebanx-plugin-activate-logger.php
+++ services/loggers/class-wc-ebanx-plugin-activate-logger.php
@@ -11,9 +11,9 @@ final class WC_EBANX_Plugin_Activate_Logger extends WC_EBANX_Logger {
 	 * @param array $log_data data to be logged.
 	 * @param string $event event name to be logged.
 	 */
-	public static function persist( array $log_data = [], $event = null) {
+	public static function persist( array $log_data = [], $event = 'plugin_activate') {
 		parent::save(
-			'plugin_activate',
+			$event,
 			array_merge(
 				WC_EBANX_Log::get_platform_info(),
 				$log_data
