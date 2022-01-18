18385f3bd7ef4a05eb522874dc049bff1ff46856
João Batista Neto <neto.joaobatista@gmail.com>
2022-01-18 10:33:26 -0300

    Update services/loggers/class-wc-ebanx-notification-query-logger.php
    Co-authored-by: Ale Bark Bruneri <ale.bark_bruneri@hotmail.com>

--- services/loggers/class-wc-ebanx-notification-query-logger.php
+++ services/loggers/class-wc-ebanx-notification-query-logger.php
@@ -10,6 +10,6 @@ final class WC_EBANX_Notification_Query_Logger extends WC_EBANX_Logger {
 	 * @param string $event event name to be logged.
 	 */
 	public static function persist( array $log_data = [], $event = null) {
-		parent::save( 'notification_query', $log_data );
+		parent::save($event, $log_data);
 	}
 }
